e70bd2ebd6accb62159aeb56c002764824ded4ca
joserosalesdh <jm.rosales92@gmail.com>
2020-07-29 13:33:21 -0300

    comit

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import logo from './logo.svg';
+import {incrementar, decrementar, setear} from './reducers'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSetear = e => {
+    const {setear} = this.props //saco setear desde las props 
+    const {valor} = this.state //El valor lo saca del estado 
+    setear(Number(valor)) //Pongo number para que me lo tome como numero y no string 
+  }
+  handleChange = e => {
+    const { name, value } = e.target 
+    this.setState({[name]: value})
+  }
+  render(){
+    const {incrementar, decrementar, valor} = this.props
+    console.log(this.state)
+    return (
+      <div className="App">
+        <p>{valor}</p>
+        <button onClick={incrementar}>Incrementar</button>
+        <button onClick={decrementar}>Decrementar</button>
+        <input name="valor" onChange={this.handleChange}/>
+        <button onClick={this.handleSetear}>Setear</button>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+const mapStateToProps = state => { //Recibe el estado completo y la principal funcion es adaptar este estado a entregarle a nuestro componente de app
+  return {
+    valor: state,
+  }
+}
+const mapDispatchToProps = dispatch =>({ // Vamos aconstruir un objeto cuya propiedades se le tiene que pasar a App, osea la propiedad que cree aca va a aparecer disponible en nuestro app
+  incrementar: () => dispatch(incrementar()),
+  decrementar: () => dispatch(decrementar()),
+  setear: payload => dispatch(setear(payload)), //Cuando ejecutemos la funcion de setear, que va a quedar disponible en las propiedades 
+  
+})
+export default connect(mapStateToProps, mapDispatchToProps) (App); // El primer parentesis es la primera vez de que nosotros la ejecutemos vamos a tener que pasarle dos argumentos 
--- redux/src/reducers/index.js
+++ redux/src/reducers/index.js
@@ -1,16 +1,33 @@
 // modulo/accion
 const INCREMENTAR = 'CONTADOR/INCREMENTAR'
 const DECREMENTAR = 'CONTADOR/DECREMENTAR'
+const SETEAR = 'CONTADOR/SETEAR'
+
+export const incrementar = () => ({
+    type: INCREMENTAR
+})
+export const decrementar = () => ({
+    type: DECREMENTAR
+})
+// Setear recibe un payload por que de esta manera nos aseguramos que los datos que vienen adjuntos en las accio nes q despachamos van a ser de payload
+export const setear = payload => ({
+    type: SETEAR,
+    payload,
+})
 
 
 // Los reducers por lo general son una funcion que reciben dos argumentos.. el primeo es el estado, pero si q la aplicación se ejecuta por primea vez tiene que recibir un estado inicial y a su vez recibe una accion
 const initialState = 0;
-export default function (state = initialState, action){
+
+// Tdos nuestros reducers tienen que retornar estados inmutables 
+export default function (state = initialState, action){ //La accion son los objetos que se encuentran dentro de las const osea los types 
     switch (action.type) {
         case INCREMENTAR:
             return state + 1
         case DECREMENTAR:
             return state - 1
+        case SETEAR:
+            return action.payload    
         default:
             return state
     }
